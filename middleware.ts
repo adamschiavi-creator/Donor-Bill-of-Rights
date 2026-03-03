3ab01e0a7e99da24e1936fe667414ae2728def85
adamschiavi-creator <adam.schiavi@gmail.com>
2026-03-03 15:10:49 -0500

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { SESSION_COOKIE, hashPasswordEdge } from "@/lib/auth";
 
-const PUBLIC_PATHS = ["/login", "/api/auth"];
+const PUBLIC_PATHS = ["/login", "/api/"];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
