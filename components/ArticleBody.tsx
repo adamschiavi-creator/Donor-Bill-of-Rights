33c08297689c1382cb6c7d4490c8530e8466eb64
adamschiavi-creator <adam.schiavi@gmail.com>
2026-03-03 14:48:42 -0500

    Add files via upload

--- components/ArticleBody.tsx
+++ components/ArticleBody.tsx
@@ -6,6 +6,9 @@ import Article4Body from "./article-bodies/Article4";
 import Article5Body from "./article-bodies/Article5";
 import Article6Body from "./article-bodies/Article6";
 import Article7Body from "./article-bodies/Article7";
+import Article8Body from "./article-bodies/Article8";
+import Article9Body from "./article-bodies/Article9";
+import Article10Body from "./article-bodies/Article10";
 
 const bodyMap: Record<number, React.ComponentType> = {
   1: Article1Body,
@@ -15,6 +18,9 @@ const bodyMap: Record<number, React.ComponentType> = {
   5: Article5Body,
   6: Article6Body,
   7: Article7Body,
+  8: Article8Body,
+  9: Article9Body,
+  10: Article10Body,
 };
 
 interface Props {
